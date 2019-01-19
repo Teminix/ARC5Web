5b6b0f746697a2441507b100dd95b1b57e5fb2cc
Chanakya M <mchanakya@gmail.com>
2019-01-19 13:56:32 +0530

    Update codefest_actual_code.ino

--- Arduino Code/codefest_actual_code/codefest_actual_code.ino
+++ Arduino Code/codefest_actual_code/codefest_actual_code.ino
@@ -9,6 +9,14 @@
 #define DHTTYPE DHT11
 #define dht_dpin 4  //digital pin 4 for data output of the Temperature sensor
 
+const char *ssid = "oakridge";
+const char *pass = "Oak@Blr123";
+
+String page = "";
+String text = ""; 
+String data = "";
+
+
 void setup() {
   
 }
