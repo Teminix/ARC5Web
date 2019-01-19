9e4c63c91047d13b18ab72e6ce597405593b052c
Chanakya M <mchanakya@gmail.com>
2019-01-19 12:04:00 +0530

    Create codefest_actual_code.ino

---
+++ Arduino Code/codefest_actual_code/codefest_actual_code.ino
@@ -0,0 +1,19 @@
+#include <WiFiServer.h>
+#include <ESP8266HTTPClient.h>
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+
+#define DHTTYPE DHT11
+#define dht_dpin 4  //digital pin 4 for data output of the Temperature sensor
+
+void setup() {
+  
+}
+
+void loop() {
+
+
+}
